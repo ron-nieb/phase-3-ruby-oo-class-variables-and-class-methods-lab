29cc8ea55b16f2fc6b3c6c851afd1fba709c1ade
Ronald Barasa <barasaogoro@gmail.com>
2023-05-22 11:49:22 +0300

    completed test

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] = @@genres.count(genre)
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      artist_count[artist] = @@artists.count(artist)
+    end
+    artist_count
+  end
+end
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+puts ninety_nine_problems.name
+puts ninety_nine_problems.artist
+puts ninety_nine_problems.genre
